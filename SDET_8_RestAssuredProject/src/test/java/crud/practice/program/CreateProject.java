ef1c7e6f1222210593aa7804d7243a58079c1b8c
Deepak <Deepak@Ty-Deepak-Tr>
2020-12-31 16:07:43 +0530

    push

---
+++ SDET_8_RestAssuredProject/src/test/java/crud/practice/program/CreateProject.java
@@ -0,0 +1,35 @@
+package crud.practice.program;
+
+import java.util.HashMap;
+
+import org.json.simple.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class CreateProject {
+	
+	@Test
+	public void getallProjectsTest() {
+		
+		JSONObject obj = new JSONObject();
+		obj.put("createdBy", "Manoj");
+		obj.put("projectName", "SCAnamiras");
+		obj.put("status", "Completed");
+		obj.put("teamSize", 12);
+		
+		 RequestSpecification reqs =   RestAssured.given();
+		
+		 reqs.contentType(ContentType.JSON);
+		 reqs.body(obj);
+		Response resp =  reqs.post("http://localhost:8084/addProject");
+		
+		System.out.println(resp.getStatusCode());
+		
+	}
+
+}
